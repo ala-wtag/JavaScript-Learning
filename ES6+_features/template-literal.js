e82ffa6903799e9cbd805ecf5058e92f0d6e933d
Adhara <adhara.labannya@welldev.io>
2025-08-28 19:10:37 +0600

    .

---
+++ ES6+_features/template-literal.js
@@ -0,0 +1,12 @@
+const name = "Adam";
+const age = 25;
+
+// Using template literals
+const greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(greeting);
+
+// Multi-line string
+const message = `This is a multi-line string.
+You can write it without using \n characters.
+Hello ${name}!`;
+console.log(message);
